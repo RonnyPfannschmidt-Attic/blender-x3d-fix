f13d06467c829fec06fb866bba578e94bf4d8ea3
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2011-04-20 18:30:30 +0200

    add test for ensuring ImageTexture Order

--- test_bx3d_fix.py
+++ test_bx3d_fix.py
@@ -6,7 +6,8 @@ from xml.etree.ElementTree import Element, SubElement
 def make_broken_tree():
     app = Element('Appearance')
     mat = SubElement(app, 'Material')
-    tex = SubElement(app, 'ImageTexture')
+    tex = SubElement(app, 'ImageTexture', {'idx':0})
+    tex = SubElement(app, 'ImageTexture', {'idx':1})
     return app
 
 
@@ -15,6 +16,8 @@ def test_fix_tree():
     tree = make_broken_tree()
     assert is_broken_tree(tree)
     fix_tree(tree)
+    for idx, item in enumerate(tree.findall('ImageTexture')):
+        assert item.attrib['idx'] == idx
     assert not is_broken_tree(tree)
 
 
